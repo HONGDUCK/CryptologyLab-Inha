4a01e7ad67f28c2d0f88208fc21ee0ba0191e2cd
sssongsihyeok11 <ssongsh98@naver.com>
2023-07-04 12:54:40 +0900

    test code입니다.
    num으로 n을 임시로 고정시켰고 테스트 실행한 결과 secretKeyGenerator 함수쪽에서 문제가 발생하였습니다.

---
+++ LWE.cpp
@@ -0,0 +1,16 @@
+/*<------------- Library -------------->*/
+#include <iostream>
+using namespace std;
+
+/*<----------------- function Prototype ------------------>*/
+void hello();
+
+int main(){
+
+    return 0;
+}
+
+/*<------------- function Definition -------------->*/
+void hello(){
+    cout << "hello Cryptology!\n";
+}
